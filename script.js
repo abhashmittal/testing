535ce22c3f90ac392ae5cb08991584a113b8fb37
abhashmittal <160855129+abhashmittal@users.noreply.github.com>
2025-01-28 13:47:25 +0100

    restructure

--- script.js
+++ script.js
@@ -2,65 +2,18 @@ $(document).ready(function($){
   orientationChange();
 });
 
+// Call the function with the path to the image
+toggleContentBasedOnImage('/assets/popup/scan.jpg');
+
 const viewer = new PhotoSphereViewer.Viewer({
   container: document.querySelector('#viewer'),
   panorama: 'assets/preservice6.jpg',
   defaultZoomLvl: '20',
 });
 
-
-var mybtn = document.querySelector("#button")
-var slide = document.querySelector("#slide")
-var info = document.querySelector("#info")
-var nav = document.querySelector("#nav")
-var view = document.querySelector("#viewer")
-var overlay = document.querySelector("#overlay")
-var popup = document.querySelector("#pop")
-var closepopup = document.querySelector("#button4")
-var sitenav = document.querySelector("#sitenav")
-var karte = document.querySelector(".popupbg")
-
-closepopup.addEventListener("click", function() {
-  popup.classList.toggle('active');
-  sitenav.classList.toggle('active');
-  karte.classList.toggle('active');
-});
-
-mybtn.addEventListener("click", function() {
-
-  slide.classList.toggle("show");
-  mybtn.classList.toggle('active');
-  info.classList.toggle('active');
-  nav.classList.toggle('active');
-  overlay.classList.toggle('active')
-
-});
-
-slide.addEventListener("click", function(){
-
-  if ($(".konzept:hover").length != 0){
-
-  } else {
-  slide.classList.toggle("show");
-  mybtn.classList.toggle('active');
-  info.classList.toggle('active');
-  nav.classList.toggle('active');
-  overlay.classList.toggle('active')
-}
-
-});
-
-function orientationChange() {
-  if(window.addEventListener) {
-      window.addEventListener("orientationchange", function() {
-          location.reload();
-      });
-  }
-};
-
 async function checkImageExists(imagePath) {
   try {
-    const response = await fetch(imagePath, { method: 'HEAD' });
+    var response = await fetch(imagePath, { method: 'HEAD' });
     return response.ok; // true if the image exists
   } catch (error) {
     return false; // false if there was an error (e.g., 404)
@@ -81,6 +34,52 @@ async function toggleContentBasedOnImage(imagePath) {
   }
 }
 
-// Call the function with the path to the image
-toggleContentBasedOnImage('/assets/popup/scan.jpg');
+
+closepopup.addEventListener("click", function() {
+  popup.classList.toggle('active');
+  sitenav.classList.toggle('active');
+  karte.classList.toggle('active');
+});
+
+
+var popup = document.querySelector("#pop")
+var closepopup = document.querySelector("#button4")
+var sitenav = document.querySelector("#sitenav")
+var karte = document.querySelector(".popupbg")
+
+//navigation triggers when there is no Pop-Up present
+var mybtn = document.querySelector("#button")
+var slide = document.querySelector("#slide")
+var info = document.querySelector("#info")
+var nav = document.querySelector("#nav")
+var view = document.querySelector("#viewer")
+var overlay = document.querySelector("#overlay")
+
+
+mybtn.addEventListener("click", function() {
+  slide.classList.toggle("show");
+  mybtn.classList.toggle('active');
+  info.classList.toggle('active');
+  nav.classList.toggle('active');
+  overlay.classList.toggle('active')
+});
+
+slide.addEventListener("click", function(){
+  if ($(".konzept:hover").length != 0){
+  } else {
+  slide.classList.toggle("show");
+  mybtn.classList.toggle('active');
+  info.classList.toggle('active');
+  nav.classList.toggle('active');
+  overlay.classList.toggle('active')
+}
+});
+
+function orientationChange() {
+  if(window.addEventListener) {
+      window.addEventListener("orientationchange", function() {
+          location.reload();
+      });
+  }
+};
 
